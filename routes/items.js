e6682fd7e8bc36b2ba5b49b4ad833a3d95b53434
CreateWater <blackrp001@naver.com>
2025-02-18 03:19:18 +0900

    USE MariaDB

--- routes/items.js
+++ routes/items.js
@@ -1,45 +1,65 @@
 var express = require('express');
 var router = express.Router();
 var movieDB = require('../databases/movieDB.js')
+var mariaDB = require('../databases/mariaDb.js')
 var dbUtills = require('../utills/db_utills.js')
 
-let db = movieDB.db
-let id = db.size
+let db = mariaDB
+db.connect();
 
 // 영화 조회
-router.get('/', function(req, res, next) {
-  screen = dbUtills.getMovie(db, id)
-  res.send(screen);
+router.get('/', async (req, res, next) => {
+  try {
+    let screen = await dbUtills.getMovie(db)
+    res.send(screen);
+  } catch (error) {
+
+  }
+  
 });
 
 // 영화 등록
-router.post('/', (req, res) => {
-  id++
-  dbUtills.postMovie(req, db, id)
-  screen = dbUtills.getMovie(db, id)
-  res.send(screen);
+router.post('/', async (req, res) => {
+  dbUtills.postMovie(req, db)
+  try {
+    let screen = await dbUtills.getMovie(db)
+    res.send(screen);
+  } catch (error) {
+  }
 })
 
 // 특정 영화 조회
-router.get('/:id', (req, res) => {
-  let id = parseInt(req.params.id)
-  let screen = dbUtills.getMovieDetail(db, id)
-  if (screen || (screen == "")){
+router.get('/:id', async (req, res) => {
+  try {
+    let id = parseInt(req.params.id)
+    let screen = await dbUtills.getMovieDetail(db, id)
+    if (screen){
     res.json(screen)
-  } else {
+    } else {
     res.send('404')
+    }
+  } catch (error) {
+    
   }
+  
 }) 
 
 // 특정 영화 수정
-router.post('/:id', (req, res) => {
-  let id = parseInt(req.params.id)
-  if (dbUtills.postMovie(req, db, id, true)){  
-    let screen = dbUtills.getMovieDetail(db, id)
-    res.json(screen)
+router.put('/:id', async (req, res) => {
+  try {
+    let id = parseInt(req.params.id)
+    let valid  = await dbUtills.putMovie(req, db, id)
+    console.log(valid)
+    if (valid){  
+      let screen = await dbUtills.getMovieDetail(db, id)
+      res.json(screen)
   } else {
-    // 404
-    res.send('404')
+      // 404
+      res.send('404')
+  }
+    
+  } catch (error) {
+    
   }
 
 }) 
--- utills/db_utills.js
+++ utills/db_utills.js
@@ -1,38 +1,119 @@
-function postMovie(req, db, id, checkExist=false){
-    if (checkExist == true){
-        if (db.has(id) == false){
-            return false;
-        }
-    }
-    db.set(id, req.body)
-    console.log(`${db.get(id).title}이 등록되었습니다.`)
-    return true;
+const { query } = require("express");
+
+function postMovie(req, db){
+    let movie = req.body
+    db.query(`INSERT INTO items (title, author ,year, genre, summary)
+    VALUES ('${movie.title}', '${movie.author}', ${movie.year}, ${movie.genre}, '${movie.summary}');`, (err, rows) => {
+        if (err) {
+            console.error('Error inserting movie:', err);
+            return;
+        } 
+        console.log(`${movie.title}이 등록되었습니다.`);
+        console.log(rows);
+    });
 }
 
-function getMovie(db){
-    let txt =  "List\n---\n"
-    db.forEach((value, key) => {
-        txt += `${key}: ${value.title}\n`;
+function putMovie(req, db, id){
+    return new Promise((resolve, reject) => {
+        db.query(`SELECT * FROM items WHERE id=${id}`, (err, rows) => {
+            if (err) {
+                console.error('Error fetching data:', err);
+                reject('Error fetching data');
+                return;
+            }
+            
+            let movie = req.body
+            db.query(`UPDATE items SET title='${movie.title}', author='${movie.author}', year=${movie.year},genre=${movie.genre},summary='${movie.summary}' WHERE id=${id}`, (err, rows) => {
+
+                if (err) {
+                    console.error('Error fetching data:', err);
+                    reject('Error fetching data');
+                    return;
+                }
+
+                if ((rows.length === 0)){
+                    resolve(false);
+                }
+                console.log(`${movie.title}이 수정되었습니다.`);
+                console.log(rows);
+                console.log(true)
+                resolve(true);
+            });
+
+        });
+
     });
-    console.log(txt)
-    return txt
+}
+
+
+
+function getMovie(db){
+    return new Promise((resolve, reject) => {
+        let txt =  "List\n---\n"
+        db.query("SELECT id, title From items", (err, rows) => {
+
+            if (err) {
+                console.error('Error fetching data:', err);
+                reject('Error fetching data');
+                return;
+            }
+
+            let queryArray = rows.map(row => ({ id: row.id, title: row.title }));
+
+            queryArray.forEach( element => {
+            console.log('Element: ', element);
+            txt += `${element.id}: ${element.title}\n`;
+        });
+
+        console.log(txt);
+            resolve(txt);
+
+        });
+    });
 }
 
 function getMovieDetail(db, id){
-    if (db.has(id)){
-        return db.get(id)
-    } else {
-        return false
-    }
+    return new Promise((resolve, reject) => {
+        db.query(`SELECT * From items WHERE id=${id}`, (err, rows) => {
+            if (err) {
+                console.error('Error fetching data:', err);
+                reject('Error fetching data');
+                return;
+            }
+            if (rows.length === 0){
+                resolve(false)
+            } else {
+                console.log(rows)
+                resolve(rows[0])
+            }
+        });
+    });
 }
 
 function deleteMovie(db, id){
-    if (db.has(id)){
-        db.delete(id)
-        return true
-    } else {
-        return false
-    }
+    return new Promise((resolve, reject) => {
+        db.query(`SELECT * From items WHERE id=${id}`, (err, rows) => {
+            if (err) {
+                console.error('Error fetching data:', err);
+                reject('Error fetching data');
+                return;
+            }
+            if (rows.length === 0){
+                resolve(false)
+            } else {
+                db.query(`DELETE From items WHERE id=${id}`, (err, rows) =>{
+                    if (err) {
+                        console.error('Error fetching data:', err);
+                        reject('Error fetching data');
+                        return;
+                    }
+                    resolve(true)
+                });
+
+            }
+        });
+    });
+    
 }
 
-module.exports = { postMovie, getMovie, getMovieDetail, deleteMovie };
+module.exports = { postMovie, putMovie ,getMovie, getMovieDetail, deleteMovie };
